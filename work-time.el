42e49ac544cad423c8f5e0ba681607b0dfd584f2
JenChieh <jcs090218@gmail.com>
2021-02-07 00:52:18 +0800

    Add core

---
+++ work-time.el
@@ -0,0 +1,42 @@
+;;; work-time.el --- How long have you been working?  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-02-07 00:46:00
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: How long have you been working?
+;; Keyword: utility work time
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs-elpa/work-time
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; How long have you been working?
+;;
+
+;;; Code:
+
+(defgroup work-time nil
+  "How long have you been working?"
+  :prefix "work-time-"
+  :group 'tool
+  :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/work-time"))
+
+(provide 'work-time)
+;;; work-time.el ends here
